a1736bab827a8bb221b746743d406ed040b6d1ab
Zack Bartel <zack@bartel.com>
2019-10-24 16:33:11 -0700

    up

--- main.go
+++ main.go
@@ -1,38 +1,54 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"time"
 
 	"github.com/zackb/go-appstoreconnect/appstoreconnect"
 	"github.com/zackb/go-appstoreconnect/encoding"
 )
 
+const (
+	CmdSalesReport   string = "SalesReport"
+	CmdFinanceReport string = "FinanceReport"
+)
+
 type cmd struct {
+	service         string
 	credentialsFile string
 	outputFormat    encoding.Encoding
 }
 
 func main() {
-	c, _ := parseCmd()
+	c, err := parseCmd()
+	if err != nil {
+		flag.Usage()
+		return
+	}
 	client, err := appstoreconnect.NewClientFromCredentialsFile(c.credentialsFile)
 	checkError(err)
 
-	// financeReport(client, c)
-	salesReport(client, c)
+	switch c.service {
+	case CmdSalesReport:
+		salesReport(client, c)
+	case CmdFinanceReport:
+		financeReport(client, c)
+	default:
+		flag.Usage()
+	}
 }
 
-/*
 func financeReport(client *appstoreconnect.Client, c *cmd) {
-	d, err := client.GetFinanceReport(time.Now(), "US")
+	d, err := client.FinanceReport.Get(time.Now(), "US")
 	checkError(err)
 	fmt.Println(string(d))
 }
-*/
 
 func salesReport(client *appstoreconnect.Client, c *cmd) {
 	/*
-		d, err := client.GetSalesReport(
+		d, err := client.SalesReport.Get(
 			time.Now().AddDate(0, -2, 0),
 			appstoreconnect.Weekly,
 			appstoreconnect.ReportSales,
@@ -66,8 +82,11 @@ func parseCmd() (*cmd, error) {
 	c := cmd{}
 	flag.StringVar(&c.credentialsFile, "c", "credentials.yml", "path to credentials yaml file")
 	flag.Var(&c.outputFormat, "o", "output format")
-
 	flag.Parse()
+	c.service = flag.Arg(0)
+	if c.service == "" {
+		return nil, errors.New("Must specify a command")
+	}
 	return &c, nil
 }
 
